914fa6bed692a1368d84877e1c89fe27b6e61721
Scot M <scotmoor@microsoft.com>
2016-09-02 14:21:12 -0700

    minor fixes. split out intial configuration and start up of spinnaker from credentials setup script

--- azure-spinnaker/scripts/initAzureSpinnaker.sh
+++ azure-spinnaker/scripts/initAzureSpinnaker.sh
@@ -4,12 +4,20 @@ set -e
 set -o pipefail
 
 SPINNAKER_CONFIG_DIR="/opt/spinnaker/config/azure_config/"
-TARGET_FILE="set-azure-credentials.sh"
-SOURCE="https://raw.githubusercontent.com/scotmoor/azure-quickstart-templates/master/azure-spinnaker/scripts/set-azure-credentials.sh"
-FULL_PATH=$SPINNAKER_CONFIG_DIR$TARGET_FILE
+CRED_FILE="set-azure-credentials.sh"
+INIT_FILE="configure_spinnaker.sh"
+CRED_SOURCE="https://raw.githubusercontent.com/scotmoor/azure-quickstart-templates/base_vmparam/azure-spinnaker/scripts/$CRED_FILE"
+INIT_SOURCE="https://raw.githubusercontent.com/scotmoor/azure-quickstart-templates/base_vmparam/azure-spinnaker/scripts/$CRED_FILE"
+CRED_FULL_PATH=$SPINNAKER_CONFIG_DIR$CRED_FILE
+INIT_FULL_PATH=$SPINNAKER_CONFIG_DIR$INIT_FILE
 
 # pull down the latest credential setup script
-sudo curl -o $FULL_PATH $SOURCE
+sudo curl -o $CRED_FULL_PATH $CRED_SOURCE
 
 # execute the credentials setup script
-sudo bash $FULL_PATH
+
+sudo bash $CRED_FULL_PATH
+
+sudo curl -o $INIT_FULL_PATH $INIT_SOURCE
+
+sudo bash $INIT_FULL_PATH 
--- azure-spinnaker/scripts/set-azure-credentials.sh
+++ azure-spinnaker/scripts/set-azure-credentials.sh
@@ -141,7 +141,7 @@ azure group deployment create -g $my_default_resource_group -n "$PACKER_STORAGE_
 echo "update version of packer"
 PACKER_INSALLER="packer.zip"
 sudo curl -o $my_azure_spinnaker_config_path/$PACKER_INSALLER  "https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip"
-sudo unzip $my_azure_spinnaker_config_path/$PACKER_INSALLER -d "/usr/bin" -q
+sudo unzip -o $my_azure_spinnaker_config_path/$PACKER_INSALLER -d "/usr/bin"
 
 echo " "
 echo "Default Resource Group:" $my_default_resource_group
@@ -186,44 +186,43 @@ echo " "
 echo "update rosco configuration to use credentails set above"
 echo " "
 # download the latest rosco.yml from the git repo
-ROSCO_CONFIG_SOURCE="https://raw.githubusercontent.com/scotmoor/azure-quickstart-templates/master/azure-spinnaker/scripts/rosco.yml"
-sudo curl -o -o "$my_azure_spinnaker_config_path/rosco.yml" $ROSCO_CONFIG_SOURCE
+ROSCO_CONFIG_SOURCE="https://raw.githubusercontent.com/scotmoor/azure-quickstart-templates/master/azure-spinnaker/scripts/rosco-local.yml"
+sudo curl -o "$my_azure_spinnaker_config_path/rosco-local.yml" $ROSCO_CONFIG_SOURCE
 # copy the download rosco.yml to the config dir as rosco-local.yml
-echo "sudo bash -c 'cp $my_azure_spinnaker_config_path/rosco.yml /opt/spinnaker/config/rosco-local.yml'"
-sudo bash -c 'cp /opt/spinnaker/config/azure_config/rosco.yml /opt/spinnaker/config/rosco-local.yml'
+echo "sudo bash -c 'cp $my_azure_spinnaker_config_path/rosco-local.yml /opt/spinnaker/config/rosco-local.yml'"
+sudo bash -c 'cp /opt/spinnaker/config/azure_config/rosco-local.yml /opt/spinnaker/config/rosco-local.yml'
 
-echo "Start Spinnaker"
-sudo bash -c '/opt/spinnaker/scripts/start_spinnaker.sh'
-sleep 30
+#echo "Start Spinnaker"
+#sudo bash -c '/opt/spinnaker/scripts/start_spinnaker.sh'
+#sleep 30
 
-APP_NAME="mydemoapp"
-STACK="st1"
-APPG_DETAIL="frontend"
-APP_GATEWAY_NAME="$APP_NAME-$STACK-$APPG_DETAIL"
-PIPELINE_NAME=$APP_NAME"pipeline"
-PIPELINE_DETAIL="demo"
+#APP_NAME="mydemoapp"
+#STACK="st1"
+#APPG_DETAIL="frontend"
+#APP_GATEWAY_NAME="$APP_NAME-$STACK-$APPG_DETAIL"
+#PIPELINE_NAME=$APP_NAME"pipeline"
+#PIPELINE_DETAIL="demo"
 
-echo "Demo Application Name: " $APP_NAME 
-echo "Demo Load Balancer: " $APP_GATEWAY_NAME
-echo "Demo Pipeline: " $PIPELINE_NAME
+#echo "Demo Application Name: " $APP_NAME 
+#echo "Demo Load Balancer: " $APP_GATEWAY_NAME
+#echo "Demo Pipeline: " $PIPELINE_NAME
 
 # Create an application in Spinnaker
-echo "Creating default application \"$APP_NAME\" in Spinnaker"
-APP_RESPONSE=sudo curl -X POST --header "Content-Type: application/json" --header "Accept: */*" -d "{\"job\": [ { \"type\": \"createApplication\", \"account\": \"my-azure-account\", \"application\": { \"cloudProviders\": \"azure\", \"instancePort\": \"null\", \"name\": \"$APP_NAME\", \"email\":\"spinnakeruser@outlook.com\" }, \"user\": \"[anonymous]\" }], \"application\": \"$APP_NAME\", \"description\": \"Create Application: $APP_NAME\"}" "http://localhost:8084/applications/$APP_NAME/tasks"
-echo "Demo Application $APP_NAME created"
+#echo "Creating default application \"$APP_NAME\" in Spinnaker"
+#APP_RESPONSE=sudo curl -X POST --header "Content-Type: application/json" --header "Accept: */*" -d "{\"job\": [ { \"type\": \"createApplication\", \"account\": \"my-azure-account\", \"application\": { \"cloudProviders\": \"azure\", \"instancePort\": \"null\", \"name\": \"$APP_NAME\", \"email\":\"spinnakeruser@outlook.com\" }, \"user\": \"[anonymous]\" }], \"application\": \"$APP_NAME\", \"description\": \"Create Application: $APP_NAME\"}" "http://localhost:8084/applications/$APP_NAME/tasks"
+#echo "Demo Application $APP_NAME created"
 
 # Create a load balancer (Azure App Gateway)
-echo "Creating default application gateway \"$APP_GATEWAY_NAME\" in Spinnaker"
-APPGATE_RESPONSE=sudo curl -X POST --header "Content-Type: application/json" --header "Accept: */*" -d "[ {\"upsertLoadBalancer\": {\"cloudProvider\": \"azure\",\"appName\": \"$APP_NAME\",\"loadBalancerName\": \"$APP_GATEWAY_NAME\",\"stack\": \"$STACK\",\"detail\": \"$APPG_DETAIL\",\"credentials\": \"my-azure-account\",\"region\": \"$default_location\",\"probes\": [{\"probeName\": \"healthcheck1\",\"probeProtocol\": \"HTTP\",\"probePort\": \"localhost\",\"probePath\": \"/\",\"probeInterval\": 30,\"unhealthyThreshold\": 8,\"timeout\": 120}],\"loadBalancingRules\": [{\"ruleName\": \"$APP_GATEWAY_NAME-rule0\",\"protocol\": \"HTTP\",\"externalPort\": 80,\"backendPort\": 80,\"probeName\": \"$APP_GATEWAY_NAME-probe\",\"persistence\": \"None\", \"idleTimeout\": 4 }], \"name\":\"$APP_GATEWAY_NAME\",\"user\": \"[anonymous]\"}}]" "http://localhost:7002/azure/ops"
-echo "Demo Load Balancer $APP_GATEWAY_NAME created"
-
+#echo "Creating default application gateway \"$APP_GATEWAY_NAME\" in Spinnaker"
+#APPGATE_RESPONSE=sudo curl -X POST --header "Content-Type: application/json" --header "Accept: */*" -d "[ {\"upsertLoadBalancer\": {\"cloudProvider\": \"azure\",\"appName\": \"$APP_NAME\",\"loadBalancerName\": \"$APP_GATEWAY_NAME\",\"stack\": \"$STACK\",\"detail\": \"$APPG_DETAIL\",\"credentials\": \"my-azure-account\",\"region\": \"$default_location\",\"probes\": [{\"probeName\": \"healthcheck1\",\"probeProtocol\": \"HTTP\",\"probePort\": \"localhost\",\"probePath\": \"/\",\"probeInterval\": 30,\"unhealthyThreshold\": 8,\"timeout\": 120}],\"loadBalancingRules\": [{\"ruleName\": \"$APP_GATEWAY_NAME-rule0\",\"protocol\": \"HTTP\",\"externalPort\": 80,\"backendPort\": 80,\"probeName\": \"$APP_GATEWAY_NAME-probe\",\"persistence\": \"None\", \"idleTimeout\": 4 }], \"name\":\"$APP_GATEWAY_NAME\",\"user\": \"[anonymous]\"}}]" "http://localhost:7002/azure/ops"
+#echo "Demo Load Balancer $APP_GATEWAY_NAME created"
 
 # Create a pipeline with bake and deploy stage
-CREATE_PIPELINE_DATA="{\"application\":\"$APP_NAME\",\"appConfig\":{},\"keepWaitingPipelines\":false,\"limitConcurrent\":true,\"parallel\":true,\"name\":\"$PIPELINE_NAME\",\"stages\":[{\"baseLabel\":\"release\",\"baseName\":\"test\",\"baseOs\":\"ubuntu\",\"cloudProviderType\":\"azure\",\"extendedAttributes\":{},\"name\":\"Bake\",\"osType\":\"Linux\",\"package\":\"gedit\",\"refId\":\"1\",\"regions\":[\"$default_location\"],\"requisiteStageRefIds\":[],\"type\":\"bake\",\"user\":\"[anonymous]\"},{\"clusters\":[{\"account\":\"my-azure-account\",\"application\":\"$APP_NAME\",\"capacity\":{\"max\":1,\"min\":1,\"useSourceCapacity\":false},\"cloudProvider\":\"azure\",\"detail\":\"$PIPELINE_DETAIL\",\"freeFormDetails\":\"$PIPELINE_DETAIL\",\"image\":{\"imageName\":\"\",\"isCustom\":\"true\",\"offer\":\"\",\"ostype\":\"\",\"publisher\":\"\",\"region\":\"$default_location\",\"sku\":\"\",\"uri\":\"\",\"version\":\"\"},\"interestingHealthProviderNames\":[],\"loadBalancerName\":\"$APP_GATEWAY_NAME\",\"name\":\"$APP_NAME-$STACK-$PIPELINE_DETAIL\",\"osConfig\":{\"adminPassword\":\"!Qnti**234\",\"adminUserName\":\"spinnakeruser\"},\"region\":\"$default_location\",\"selectedProvider\":\"azure\",\"sku\":{\"capacity\":1,\"name\":\"Standard_DS1_v2\",\"tier\":\"Standard\"},\"stack\":\"$STACK\",\"type\":\"createServerGroup\",\"upgradePolicy\":\"Manual\",\"user\":\"[anonymous]\",\"viewState\":{\"allImageSelection\":null,\"disableImageSelection\":true,\"disableStrategySelection\":false,\"hideClusterNamePreview\":false,\"imageId\":null,\"instanceProfile\":\"custom\",\"loadBalancersConfigured\":true,\"mode\":\"createPipeline\",\"readOnlyFields\":{},\"securityGroupsConfigured\":true,\"submitButtonLabel\":\"Add\",\"templatingEnabled\":true,\"useAllImageSelection\":false,\"usePreferredZones\":true,\"useSimpleCapacity\":true}}],\"name\":\"Deploy\",\"refId\":\"2\",\"requisiteStageRefIds\":[\"1\"],\"type\":\"deploy\"}],\"triggers\":[]}"
-sudo curl -X POST --header "Content-Type: application/json" --header "*/*" -d "$CREATE_PIPELINE_DATA" "http://localhost:8084/pipelines"
-echo "Demo pipelname $PIPELINE_NAME created"
+#CREATE_PIPELINE_DATA="{\"application\":\"$APP_NAME\",\"appConfig\":{},\"keepWaitingPipelines\":false,\"limitConcurrent\":true,\"parallel\":true,\"name\":\"$PIPELINE_NAME\",\"stages\":[{\"baseLabel\":\"release\",\"baseName\":\"test\",\"baseOs\":\"ubuntu\",\"cloudProviderType\":\"azure\",\"extendedAttributes\":{},\"name\":\"Bake\",\"osType\":\"Linux\",\"package\":\"gedit\",\"refId\":\"1\",\"regions\":[\"$default_location\"],\"requisiteStageRefIds\":[],\"type\":\"bake\",\"user\":\"[anonymous]\"},{\"clusters\":[{\"account\":\"my-azure-account\",\"application\":\"$APP_NAME\",\"capacity\":{\"max\":1,\"min\":1,\"useSourceCapacity\":false},\"cloudProvider\":\"azure\",\"detail\":\"$PIPELINE_DETAIL\",\"freeFormDetails\":\"$PIPELINE_DETAIL\",\"image\":{\"imageName\":\"\",\"isCustom\":\"true\",\"offer\":\"\",\"ostype\":\"\",\"publisher\":\"\",\"region\":\"$default_location\",\"sku\":\"\",\"uri\":\"\",\"version\":\"\"},\"interestingHealthProviderNames\":[],\"loadBalancerName\":\"$APP_GATEWAY_NAME\",\"name\":\"$APP_NAME-$STACK-$PIPELINE_DETAIL\",\"osConfig\":{\"adminPassword\":\"!Qnti**234\",\"adminUserName\":\"spinnakeruser\"},\"region\":\"$default_location\",\"selectedProvider\":\"azure\",\"sku\":{\"capacity\":1,\"name\":\"Standard_DS1_v2\",\"tier\":\"Standard\"},\"stack\":\"$STACK\",\"type\":\"createServerGroup\",\"upgradePolicy\":\"Manual\",\"user\":\"[anonymous]\",\"viewState\":{\"allImageSelection\":null,\"disableImageSelection\":true,\"disableStrategySelection\":false,\"hideClusterNamePreview\":false,\"imageId\":null,\"instanceProfile\":\"custom\",\"loadBalancersConfigured\":true,\"mode\":\"createPipeline\",\"readOnlyFields\":{},\"securityGroupsConfigured\":true,\"submitButtonLabel\":\"Add\",\"templatingEnabled\":true,\"useAllImageSelection\":false,\"usePreferredZones\":true,\"useSimpleCapacity\":true}}],\"name\":\"Deploy\",\"refId\":\"2\",\"requisiteStageRefIds\":[\"1\"],\"type\":\"deploy\"}],\"triggers\":[]}"
+#sudo curl -X POST --header "Content-Type: application/json" --header "*/*" -d "$CREATE_PIPELINE_DATA" "http://localhost:8084/pipelines"
+#echo "Demo pipelname $PIPELINE_NAME created"
 
-echo "Spinnaker demo setup complete"
+#echo "Spinnaker demo setup complete"
 
 # TODO
 # Re-enable keyvault

a1f0980b5d334493dc5eef6430075a7465d5e461
Scot M <scotmoor@microsoft.com>
2016-09-07 16:43:17 -0700

    revert to previous version of initial script

--- azure-spinnaker/scripts/initAzureSpinnaker.sh
+++ azure-spinnaker/scripts/initAzureSpinnaker.sh
@@ -4,21 +4,12 @@ set -e
 set -o pipefail
 
 SPINNAKER_CONFIG_DIR="/opt/spinnaker/config/azure_config/"
-REPO="https://raw.githubusercontent.com/scotmoor/azure-quickstart-templates/basevm_param/azure-spinnaker/scripts/"
-CRED_FILE="set-azure-credentials.sh"
-INIT_FILE="configure_spinnaker.sh"
-CRED_SOURCE=$REPO$CRED_FILE
-INIT_SOURCE=$REPO$INIT_FILE
-CRED_FULL_PATH=$SPINNAKER_CONFIG_DIR$CRED_FILE
-INIT_FULL_PATH=$SPINNAKER_CONFIG_DIR$INIT_FILE
+TARGET_FILE="set-azure-credentials.sh"
+SOURCE="https://raw.githubusercontent.com/scotmoor/azure-quickstart-templates/basevm_param/azure-spinnaker/scripts/set-azure-credentials.sh"
+FULL_PATH=$SPINNAKER_CONFIG_DIR$TARGET_FILE
 
 # pull down the latest credential setup script
-sudo curl -o $CRED_FULL_PATH $CRED_SOURCE
+sudo curl -o $FULL_PATH $SOURCE
 
 # execute the credentials setup script
-
-sudo bash $CRED_FULL_PATH
-
-sudo curl -o $INIT_FULL_PATH $INIT_SOURCE
-
-sudo bash $INIT_FULL_PATH 
+sudo bash $FULL_PATH
